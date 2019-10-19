5c4d07ac8fb54e97b96c2d403ae53ccbc485b715
chantal <chanty98@hotmail.com>
2019-10-19 17:56:25 +0200

    add archiving feature

---
+++ src/archive.ts
@@ -0,0 +1,37 @@
+import { WebClient } from '@slack/web-api';
+import { inspect } from 'util';
+
+export async function archive() {
+  const client = new WebClient(process.env.SLACK_TOKEN);
+  const channels = await getChannels(client);
+
+  for (const channel of channels as any[]) {
+    channel.messages = [];
+    const { messages } = await client.conversations.history({
+      channel: channel.id,
+    });
+
+    for (const message of messages as any[]) {
+      if (message.reply_count) {
+        const { messages } = await client.conversations.replies({
+          channel: channel.id,
+          ts: message.ts,
+        });
+        const [thread, ...replies] = messages as any[];
+        message.replies = replies;
+      }
+
+      channel.messages.push(message);
+    }
+  }
+
+  return channels;
+}
+
+async function getChannels(client: WebClient) {
+  const { channels } = await client.conversations.list({
+    types: 'private_channel,public_channel,mpim,im',
+  });
+
+  return channels;
+}
